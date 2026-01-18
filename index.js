4b4fb91f90c8ed63eb98ba22cc5f1ef734de14ea
getSohelRana <getsohelinfo@gmail.com>
2026-01-19 01:37:57 +0600

    requested partner delele api

--- index.js
+++ index.js
@@ -261,6 +261,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE : delete partner request
+    app.delete("/partnerCounts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await partnerCountCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // UPDATE : update student api
     app.patch("/students/:id", async (req, res) => {
       const id = req.params.id;
